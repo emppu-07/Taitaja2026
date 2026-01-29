e76644ff8c1b3671dcc21753f5d5ac6355975da4
JA-P07 <julius.palttala@edu.tampere.fi>
2026-01-29 13:23:04 +0200

    adjustments

--- prototype-production/Taitaja2026/Assets/Scripts/JumpPad.cs
+++ prototype-production/Taitaja2026/Assets/Scripts/JumpPad.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    private float bounce = 20f;
+    private float bounce = 20f; //adjust jump height
 
     private void OnCollisionEnter(Collision collision)
     {
